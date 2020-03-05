02ee5d6c265908102321ca9012299d36fc7fade6
Nicholas Porter <nicholas@metax.io>
2020-03-04 22:01:03 -0800

    Created launch type in schema

---
+++ start/server/src/schema.js
@@ -0,0 +1,12 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+type Launch {
+    id: ID!
+    site: String
+    mission: Mission
+    rocket: Rocket
+    isBooked: Boolean!
+  }`;
+
+module.exports = typeDefs;
